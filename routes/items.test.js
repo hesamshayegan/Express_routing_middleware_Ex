77185504f4b36ad775b069ba73ec2a8b67d5b219
hesam <hesam@LAPTOP-K4EHTU8F>
2023-07-05 14:14:30 -0700

    add app.js, routes & test files

---
+++ routes/items.test.js
@@ -0,0 +1,66 @@
+process.env.NODE_ENV = "test";
+// npm packages
+const request = require("supertest");
+// app imports
+const app = require("../app");
+
+let items = require("../fakeDb")
+
+let item = { name: "silly", price:200 }
+
+beforeEach(async () => {
+  items.push(item)
+});
+
+afterEach(async () => {
+  items = []
+});
+// end afterEach
+
+/** GET /items - returns `{items: [item, ...]}` */
+
+describe("GET /items", function () {
+    test("Gets a list of items", async function () {
+      const response = await request(app).get(`/items`);
+      const { items } = response.body;
+      expect(response.statusCode).toBe(200);
+      expect(items).toHaveLength(1);
+    });
+  });
+//end
+
+/** GET /items/[name] - return data about one item: `{item: item}` */
+
+describe("GET /items/:name", function () {
+    test("Gets a single item", async function () {
+      const response = await request(app).get(`/items/${item.name}`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.item).toEqual(item);
+    });
+  
+    test("Responds with 404 if can't find item", async function () {
+      const response = await request(app).get(`/items/0`);
+      expect(response.statusCode).toBe(404);
+    });
+  });
+// end
+
+
+/** POST /items - create item from data; return `{item: item} */
+
+describe("POST /items", function () {
+  test("Creates a new item", async function () {
+    const response = await request(app)
+      .post(`/items`)
+      .send({
+        name: "Taco",
+        price: 0
+      })
+    expect(response.statusCode).toBe(200);
+    expect(response.body.item).toHaveProperty("name");
+    expect(response.body.item).toHaveProperty("price");
+    expect(response.body.item.name).toEqual("Taco"); 
+    expect(response.body.item.price).toEqual(0);
+  });
+});
+//end
